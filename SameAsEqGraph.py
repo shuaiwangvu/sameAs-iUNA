03c994bf0f74141d98ed576f4df7acbbcf2a7b58
shuaiwangvu <shuai.wang.vu@gmail.com>
2021-07-14 20:50:54 +0200

    input - solve - output

--- SameAsEqGraph.py
+++ SameAsEqGraph.py
@@ -1,6 +1,6 @@
 # this is an abstract class
-
-
+import networkx as nx
+import csv
 
 class SameAsEqGraph:
 
@@ -9,20 +9,27 @@ class SameAsEqGraph:
 
 class SameAsEqInputGraph(SameAsEqGraph):
 
-	def __init__(self):
+	def __init__(self, path_to_graph):
 		super().__init__()
+		self.graph = nx.Graph()
+		eq_file = open(path_to_graph, 'r')
+		reader = csv.DictReader(eq_file)
+		for row in reader:
+			s = row["SUBJECT"]
+			o = row["OBJECT"]
+			self.graph.add_edge(s, o)
 
 	def get_nodes(self):
-		return [1,2,3]
+		return self.graph.nodes()
 
 
 class SameAsEqResultGraph(SameAsEqGraph):
 	def __init__(self):
-		self.subgraphs = [1,2,3]
+		self.subgraphs = []
 		super().__init__()
 
 	def get_nodes(self):
 		nodes = []
 		for sg in self.subgraphs:
-			nodes.append(sg*2)
+			nodes += sg.nodes()
 		return nodes
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 # this is the class where SameAsEqSolver is defined
+import networkx as nx
 from SameAsEqGraph import SameAsEqGraph, SameAsEqInputGraph, SameAsEqResultGraph
 
 
 # define a class of graph solver
 class GraphSolver():
-	g = SameAsEqInputGraph()
-	h = SameAsEqResultGraph()
+	# configure the weight file
+	# location of each node
+
+	def __init__(self, path_to_graph):
+		self.input_graph = SameAsEqInputGraph(path_to_graph)
+		self.pos = nx.spring_layout(self.input_graph.graph)
+		self.result_graph = SameAsEqResultGraph()
+
+
+	def solve (self):
+		cp  = self.input_graph.graph.copy()
+		self.result_graph.subgraphs.append(cp)
+		print ('solving, resulting subgraphs =',len(self.result_graph.subgraphs))
 
 # main
-gs = GraphSolver()
-print (gs.g.get_nodes())
-print (gs.h.get_nodes())
+gs = GraphSolver('./Evaluate_May/11116_edges_original.csv')
+print (len(gs.input_graph.get_nodes()))
+gs.solve()
+print (len(gs.result_graph.get_nodes()))
