b375c4808097f464cc9b39987133f5e89f0234d4
shuaiwangvu <shuai.wang.vu@gmail.com>
2021-07-15 15:02:20 +0200

    short IRI for visualisation

--- SameAsEqGraph.py
+++ SameAsEqGraph.py
@@ -1,23 +1,64 @@
 # this is an abstract class
 import networkx as nx
-import csv
+import pandas as pd
+import tldextract
 
 class SameAsEqGraph:
 
 	def get_nodes(self):
 		pass
+	def get_edges(self):
+		pass
+
+
+def simp(e):
+	# simplify this uri by introducing the namespace abbreviation
+	ext = tldextract.extract(e)
+	# ExtractResult(subdomain='af', domain='dbpedia', suffix='org')
+
+	if 'dbpedia' == ext.domain and ext.subdomain != '' and ext.subdomain != None:
+		namespace = ext.subdomain +'.'+ext.domain
+	else :
+		namespace = ext.domain
+
+	short_IRI = ''
+
+	if e.split('#') == [e] :
+		if e.split('/') != [e]:
+			name = e.split('/')[-1]
+	else:
+		name = e.split('/')[-1]
+
+	if len (name) < 10:
+		short_IRI  = namespace + ':' + name # this can be shortened !!!
+	else:
+		short_IRI = namespace + ':' + name[:10] + '...' # this can be shortened !!!
+
+	print ('\n\noriginal e = ', e)
+	print ('short_IRI =  ', short_IRI)
+
+	return short_IRI
+
+
 
 class SameAsEqInputGraph(SameAsEqGraph):
 
 	def __init__(self, path_to_graph):
 		super().__init__()
 		self.graph = nx.Graph()
-		eq_file = open(path_to_graph, 'r')
-		reader = csv.DictReader(eq_file)
-		for row in reader:
-			s = row["SUBJECT"]
-			o = row["OBJECT"]
-			self.graph.add_edge(s, o)
+		got_data = pd.read_csv(path_to_graph)
+		sources = got_data['SUBJECT']
+		targets = got_data['OBJECT']
+		# simplify the label
+		sources_simplified = map(simp, sources)
+		targets_simplified = map(simp, targets)
+		edge_data = zip(sources_simplified, targets_simplified)
+		for (s,t) in edge_data:
+			# print ('source: ', s)
+			# print ('simplified source: ', simp(s))
+			# print ('target: ', t)
+			self.graph.add_edge(s, t)
+
 
 	def get_nodes(self):
 		return self.graph.nodes()
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # this is the class where SameAsEqSolver is defined
 import networkx as nx
 from SameAsEqGraph import SameAsEqGraph, SameAsEqInputGraph, SameAsEqResultGraph
-
+from pyvis.network import Network
+import community
+import collections
+import matplotlib.pyplot as plt
 
 # define a class of graph solver
 class GraphSolver():
@@ -10,17 +13,40 @@ class GraphSolver():
 
 	def __init__(self, path_to_graph):
 		self.input_graph = SameAsEqInputGraph(path_to_graph)
-		self.pos = nx.spring_layout(self.input_graph.graph)
+		self.pos = nx.spring_layout(self.input_graph.graph) # for visulization
+		self.unique_name_constraints = None # weighted UNC
 		self.result_graph = SameAsEqResultGraph()
-
+		net = Network()
 
 	def solve (self):
 		cp  = self.input_graph.graph.copy()
 		self.result_graph.subgraphs.append(cp)
 		print ('solving, resulting subgraphs =',len(self.result_graph.subgraphs))
 
+	def visualize(self):
+		nt = Network('700px', '700px')
+		nt.from_nx(self.input_graph.graph)
+		# nt.enable_physics(True)
+		nt.show_buttons(filter_=['physics'])
+		nt.show('input.html')
+
+	def partition_leuven(self):
+		g = self.input_graph.graph
+		partition = community.best_partition(g)
+		values = [partition.get(node) for node in g.nodes()] # values are group ids
+		counter=collections.Counter(values)
+		print(counter)
+		sp = nx.spring_layout(g)
+		nx.draw_networkx(g, pos=sp, with_labels=False, node_size=35, node_color=values)
+		# plt.axes('off')
+		plt.show()
+
+
+
 # main
 gs = GraphSolver('./Evaluate_May/11116_edges_original.csv')
 print (len(gs.input_graph.get_nodes()))
 gs.solve()
 print (len(gs.result_graph.get_nodes()))
+gs.visualize()
+# gs.partition_leuven()
