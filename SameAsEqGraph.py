23f692d867a6b34ae701af1bb4536533f3753a43
shuaiwangvu <shuai.wang.vu@gmail.com>
2021-07-15 16:11:58 +0200

    change the data structure, using attributes group

--- SameAsEqGraph.py
+++ SameAsEqGraph.py
@@ -3,12 +3,6 @@ import networkx as nx
 import pandas as pd
 import tldextract
 
-class SameAsEqGraph:
-
-	def get_nodes(self):
-		pass
-	def get_edges(self):
-		pass
 
 
 def simp(e):
@@ -34,43 +28,7 @@ def simp(e):
 	else:
 		short_IRI = namespace + ':' + name[:10] + '...' # this can be shortened !!!
 
-	print ('\n\noriginal e = ', e)
-	print ('short_IRI =  ', short_IRI)
+	# print ('\n\noriginal e = ', e)
+	# print ('short_IRI =  ', short_IRI)
 
 	return short_IRI
-
-
-
-class SameAsEqInputGraph(SameAsEqGraph):
-
-	def __init__(self, path_to_graph):
-		super().__init__()
-		self.graph = nx.Graph()
-		got_data = pd.read_csv(path_to_graph)
-		sources = got_data['SUBJECT']
-		targets = got_data['OBJECT']
-		# simplify the label
-		sources_simplified = map(simp, sources)
-		targets_simplified = map(simp, targets)
-		edge_data = zip(sources_simplified, targets_simplified)
-		for (s,t) in edge_data:
-			# print ('source: ', s)
-			# print ('simplified source: ', simp(s))
-			# print ('target: ', t)
-			self.graph.add_edge(s, t)
-
-
-	def get_nodes(self):
-		return self.graph.nodes()
-
-
-class SameAsEqResultGraph(SameAsEqGraph):
-	def __init__(self):
-		self.subgraphs = []
-		super().__init__()
-
-	def get_nodes(self):
-		nodes = []
-		for sg in self.subgraphs:
-			nodes += sg.nodes()
-		return nodes
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # this is the class where SameAsEqSolver is defined
 import networkx as nx
-from SameAsEqGraph import SameAsEqGraph, SameAsEqInputGraph, SameAsEqResultGraph
+from SameAsEqGraph import simp
 from pyvis.network import Network
 import community
 import collections
 import matplotlib.pyplot as plt
+from random import randint
+import pandas as pd
+
+
 
 # define a class of graph solver
 class GraphSolver():
@@ -12,41 +16,67 @@ class GraphSolver():
 	# location of each node
 
 	def __init__(self, path_to_graph):
-		self.input_graph = SameAsEqInputGraph(path_to_graph)
-		self.pos = nx.spring_layout(self.input_graph.graph) # for visulization
-		self.unique_name_constraints = None # weighted UNC
-		self.result_graph = SameAsEqResultGraph()
+		# input graph
+		self.input_graph = nx.Graph()
+		got_data = pd.read_csv(path_to_graph)
+
+		sources = got_data['SUBJECT']
+		targets = got_data['OBJECT']
+		edge_data = zip(sources, targets)
+
+		for (s,t) in edge_data:
+			self.input_graph.add_node(s, short_IRI = simp(s), group = 0)
+			self.input_graph.add_node(t, short_IRI = simp(t), group = 0)
+			self.input_graph.add_edge(s, t)
+
+
+		# for visulization
+		self.pos = nx.spring_layout(self.input_graph.graph)
 		net = Network()
 
+
+		# solving: (weighted unique name constraints)
+		self.unique_name_constraints = None # weighted UNC
+		# result
+		self.result_graph = None
+
+
+
 	def solve (self):
-		cp  = self.input_graph.graph.copy()
-		self.result_graph.subgraphs.append(cp)
-		print ('solving, resulting subgraphs =',len(self.result_graph.subgraphs))
+		self.result_graph  = self.input_graph.copy()
+		print ('solve num of nodes = ', len (self.result_graph.nodes()))
 
-	def visualize(self):
-		nt = Network('700px', '700px')
-		nt.from_nx(self.input_graph.graph)
-		# nt.enable_physics(True)
-		nt.show_buttons(filter_=['physics'])
-		nt.show('input.html')
+		for node in self.result_graph.nodes():
+			self.result_graph.nodes[node]['group'] = randint(0,1)
 
-	def partition_leuven(self):
-		g = self.input_graph.graph
-		partition = community.best_partition(g)
-		values = [partition.get(node) for node in g.nodes()] # values are group ids
-		counter=collections.Counter(values)
+		values = [self.result_graph.nodes[node]['group'] for node in self.result_graph.nodes()]
+		counter = collections.Counter(values)
 		print(counter)
-		sp = nx.spring_layout(g)
-		nx.draw_networkx(g, pos=sp, with_labels=False, node_size=35, node_color=values)
-		# plt.axes('off')
-		plt.show()
+
+	# def visualize(self):
+	# 	nt = Network('700px', '700px')
+	# 	nt.from_nx(self.input_graph.graph)
+	# 	# nt.enable_physics(True)
+	# 	nt.show_buttons(filter_=['physics'])
+	# 	nt.show('input.html')
+	#
+	# def partition_leuven(self):
+	# 	g = self.input_graph.graph
+	# 	partition = community.best_partition(g)
+	# 	values = [partition.get(node) for node in g.nodes()] # values are group ids
+	# 	counter=collections.Counter(values)
+	# 	print(counter)
+	# 	sp = nx.spring_layout(g)
+	# 	nx.draw_networkx(g, pos=sp, with_labels=False, node_size=35, node_color=values)
+	# 	# plt.axes('off')
+	# 	plt.show()
 
 
 
 # main
 gs = GraphSolver('./Evaluate_May/11116_edges_original.csv')
-print (len(gs.input_graph.get_nodes()))
+print (len(gs.input_graph.nodes()))
 gs.solve()
-print (len(gs.result_graph.get_nodes()))
-gs.visualize()
+print (len(gs.result_graph.nodes()))
+# gs.visualize()
 # gs.partition_leuven()
