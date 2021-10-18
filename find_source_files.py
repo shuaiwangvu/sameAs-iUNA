82a3213563b2805fccf3a9284e6d061838ac5dcf
shuaiwangvu <shuai.wang.vu@gmail.com>
2021-10-18 18:51:50 +0200

    find how many source files are involved in finding the sameAs triples

---
+++ find_source_files.py
@@ -0,0 +1,15 @@
+# sameas_laundromat_metalink.hdt
+
+# <https://krr.triply.cc/krr/metalink/fileMD5/00c48eee800fedbe0e1e5679c35dc5d5> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://krr.triply.cc/krr/metalink/def/File> .
+
+from hdt import HDTDocument, IdentifierPosition
+
+file_name = "sameas_laundromat_metalink.hdt"
+
+type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+metalink_file = "https://krr.triply.cc/krr/metalink/def/File"
+
+hdt_weight = HDTDocument(file_name)
+(triples, cardinality) = hdt_weight.search_triples("", type, metalink_file)
+
+print ('There are ', cardinality, 'knowledge bases with sameAs triples')
